6315f09c84b07aa4a756d6f0fdba619af3590c3b
unknown <moissesvrf@gmail.com>
2023-10-18 08:55:10 -0300

    Creating Greating and Greating Controller to make a GET

---
+++ SpringBootWithKotlin/backend-project-with-spring-boot/src/main/kotlin/br/com/moisses/Greeting.kt
@@ -0,0 +1,4 @@
+package br.com.moisses
+
+class Greeting (val id: Long, val content: String) {
+}
---
+++ SpringBootWithKotlin/backend-project-with-spring-boot/src/test/kotlin/br/com/moisses/StartupTests.kt
@@ -0,0 +1,13 @@
+package br.com.moisses
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
